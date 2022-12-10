a88c0d69bcadfcb48e6e4b54673bf5619738596e
MariaTrunk <75212906+MariaTrunk@users.noreply.github.com>
2022-12-10 18:53:36 +0300

    max

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace FindMaxApp
     {
         static void Main(string[] args)
         {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+
+            Console.ReadLine(); 
         }
     }
 }
